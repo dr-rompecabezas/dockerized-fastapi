99e5a6cd3d3c0d684b0ee681e22b9fce0144ff39
Felipe Villegas <felavid@gmail.com>
2021-11-13 23:58:18 -0500

    Moved env variables to .env and config

--- app/database.py
+++ app/database.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:!P0st0911@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     try:
--- app/oauth2.py
+++ app/oauth2.py
@@ -4,27 +4,24 @@ from datetime import datetime, timedelta
 from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from dotenv import load_dotenv
-load_dotenv()
+from .config import settings
 
 oauth2_scheme = oauth2.OAuth2PasswordBearer(tokenUrl="login")
-
-SECRET_KEY = "83e79b55a5f4b690a93d6b71e350abd09188b4a1c044192ed5905d512b36d1c3"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.secret_key, algorithm=settings.algorithm)
     return encoded_jwt
 
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.secret_key,
+                             algorithms=[settings.algorithm])
         id: str = payload.get("user_id")
         if id is None:
             raise credentials_exception
