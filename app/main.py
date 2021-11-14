dd42b7ada55b2fd7c2381762aecdc2bd451a12ea
Felipe Villegas <felavid@gmail.com>
2021-11-13 22:23:06 -0500

    Removed psycopg db connection; only sqlalchemy now

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import psycopg
 from .database import engine
 from . import models
 from .routes import post, user, auth
@@ -7,14 +6,6 @@ from .routes import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-try:
-    conn = psycopg.connect()
-    print("Connected!")
-except:
-    print("I am unable to connect to the database")
-
-cur = conn.cursor(row_factory=psycopg.rows.dict_row)
 
 app.include_router(post.router)
 app.include_router(user.router)
