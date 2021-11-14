bd74f914d87f35de9af01ee41c15cbcd745532bc
Felipe Villegas <felavid@gmail.com>
2021-11-13 21:02:43 -0500

    User can only update or delete own posts

--- app/routes/post.py
+++ app/routes/post.py
@@ -37,12 +37,15 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() is None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -54,6 +57,9 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     if post_to_update is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    if post_to_update.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
