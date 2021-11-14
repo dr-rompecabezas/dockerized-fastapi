c357780fd58750e3c3ed6b5fcad7cfd8d777777f
Felipe Villegas <felavid@gmail.com>
2021-11-13 20:22:54 -0500

    Renamed variables

--- app/routes/post.py
+++ app/routes/post.py
@@ -14,46 +14,46 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    records = db.query(models.Post).all()
-    return records
+    all_posts = db.query(models.Post).all()
+    return all_posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    record = models.Post(**post.dict())
-    db.add(record)
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
+    db.add(new_post)
     db.commit()
-    db.refresh(record)
-    return record
+    db.refresh(new_post)
+    return new_post
 
 
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    record = db.query(models.Post).filter(models.Post.id == id).first()
-    if record is None:
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if post is None:
         raise HTTPException(status_code=404, detail="Post not found")
-    return record
+    return post
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    record = db.query(models.Post).filter(models.Post.id == id)
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if record.first() is None:
+    if post.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    record.delete(synchronize_session=False)
+    post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    record_query = db.query(models.Post).filter(models.Post.id == id)
-    record = record_query.first()
-    if record is None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_to_update = post_query.first()
+    if post_to_update is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    record_query.update(post.dict(), synchronize_session=False)
+    post_query.update(post.dict(), synchronize_session=False)
     db.commit()
-    return record_query.first()
+    return post_query.first()
