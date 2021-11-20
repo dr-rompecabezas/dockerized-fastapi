edb805a28f6aebf1b15421def56ed58d91d70362
Felipe Villegas <felavid@gmail.com>
2021-11-20 09:50:19 -0500

    Changed test fixtures to module scope

--- tests/database.py
+++ tests/database.py
@@ -15,7 +15,7 @@ TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -26,7 +26,7 @@ def session():
         db.close()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client(session):
     def override_get_db():
         try:
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,9 +3,17 @@ from .database import client, session
 
 def test_create_user(client):
     response = client.post('/users/', json={
-        "email": "abuelita@gmail.com",
+        "email": "test_user@gmail.com",
         "password": "abc123"
     })
     new_user = schemas.User(**response.json())
-    assert new_user.email == "abuelita@gmail.com"
+    assert new_user.email == "test_user@gmail.com"
     assert response.status_code == 201
+
+
+def test_login_user(client):
+    response = client.post('/login', data={
+        "username": "test_user@gmail.com",
+        "password": "abc123"
+    })
+    assert response.status_code == 200
