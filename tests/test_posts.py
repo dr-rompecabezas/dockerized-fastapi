96febb4a9fdde6b11339c757bd69c24260b2d7b4
Felipe Villegas <felavid@gmail.com>
2021-11-20 22:53:59 -0500

    Formatted and documented code

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,12 +1,17 @@
 from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
+    """
+    Test that we can get all posts.
+    'test_posts' are sqlalchemy objects, not json objects.
+    Validate response data using schema and turn into a list of dictionaries.
+    """
     response = authorized_client.get("/posts/")
     def validate(post):
         return schemas.PostVotes(**post)
     posts_map = map(validate, response.json())
     posts_list = list(posts_map)
-    
+
     assert response.status_code == 200
     assert len(response.json()) == len(test_posts)
     assert posts_list[0].Post.id == test_posts[0].id
--- tests/test_users.py
+++ tests/test_users.py
@@ -5,16 +5,24 @@ from app.config import settings
 
 
 def test_create_user(client):
+    """
+    Test that we can create a user.
+    """
     response = client.post('/users/', json={
         "email": "test_user@gmail.com",
         "password": "abc123"
     })
     new_user = schemas.User(**response.json())
+
     assert response.status_code == 201
     assert new_user.email == "test_user@gmail.com"
 
 
 def test_login_user(client, test_user):
+    """
+    Test that we can login a user and get a valid token in the response.
+    Decode the token and validate the claims.
+    """
     response = client.post('/login', data={
         "username": test_user['email'],
         "password": test_user['password']
@@ -23,6 +31,7 @@ def test_login_user(client, test_user):
     payload = jwt.decode(login_response.access_token, settings.secret_key,
                          algorithms=[settings.algorithm])
     id = payload.get('user_id')
+
     assert response.status_code == 200
     assert id == test_user['id']
     assert login_response.token_type == "bearer"
@@ -35,9 +44,13 @@ def test_login_user(client, test_user):
     ('test_user@gmail.com', None, 422)
 
 ])
-def test_login_user_invalid_password(client, test_user, email, password, status_code):
+def test_login_user_invalid_password(client, email, password, status_code):
+    """
+    Test that we cannot login an invalid user.
+    """
     response = client.post('/login', data={
         "username": email,
         "password": password
     })
+    
     assert response.status_code == status_code
