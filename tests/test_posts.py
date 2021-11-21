13450f6d8676972cade6628d026dbf483bcb7798
Felipe Villegas <felavid@gmail.com>
2021-11-20 22:42:47 -0500

    First test for posts written

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,6 +1,12 @@
-
+from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
-    assert len(response.json()) == 3
+    def validate(post):
+        return schemas.PostVotes(**post)
+    posts_map = map(validate, response.json())
+    posts_list = list(posts_map)
+    
     assert response.status_code == 200
+    assert len(response.json()) == len(test_posts)
+    assert posts_list[0].Post.id == test_posts[0].id
